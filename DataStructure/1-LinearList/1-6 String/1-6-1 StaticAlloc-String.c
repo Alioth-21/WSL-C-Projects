b5f83d32b7f45eb40288663f5c5627b97578fbd7
Alioth <alioth21@qq.com>
2026-08-16 07:27:55 +0800

    update

--- DataStructure/1-LinearList/1-6 String/1-6-1 StaticAlloc-String.c
+++ DataStructure/1-LinearList/1-6 String/1-6-1 StaticAlloc-String.c
@@ -1,5 +1,6 @@
 // 1-6-1 StaticAlloc-String
 // Static Storage Representation
+// String Last Element is '\0' and Length: [0, MAXSIZE-1]
 
 #include<stdio.h>
 #include<stdbool.h>
@@ -11,23 +12,141 @@ typedef struct{
 }Str, *PtrS;
 
 void InitStr(PtrS S);
+bool StrEmpty(PtrS S);
+int StrLength(PtrS S);
 void PrintStr(PtrS S);
+void InputStr(PtrS S);
+void ClearStr(PtrS S);
 
 bool StrAssign(PtrS S, char* str);
 bool StrCopy(PtrS S, PtrS T);
-bool StrEmpty(PtrS S);
-
-int StrCompare(PtrS S, PtrS T);
-int StrLength(PtrS S);
-bool SubStr(PtrS Sub, PtrS S, int pos, int len);
-
+int StrCompare(PtrS S, PtrS T);                     // 字典序比较字符串S和T的大小，若S>T则返回正数，若S<T则返回负数，若S=T则返回0
+bool SubStr(PtrS Sub, PtrS S, int pos, int len);    // 子串Sub为串S的第pos个字符起长度为len的子串
 bool Concat(PtrS S, PtrS S1, PtrS S2);
-int Index(PtrS S, PtrS T);
-void ClearStr(PtrS S);
-void DestoryStr(PtrS S);
+// int Index(PtrS S, PtrS T);   // 返回子串T在主串S中的位置，若不存在则返回0
+
 
 
 
 int main(){
+    Str s1, s2, s3;
+    InitStr(&s1);
+    InitStr(&s2);
+    InitStr(&s3);
+
+    //input and print
+    InputStr(&s1);
+    PrintStr(&s1);
+
+    InputStr(&s2);
+    PrintStr(&s2);
+    ClearStr(&s2);
+    PrintStr(&s2);
+
+    // copy
+    StrCopy(&s1, &s3);
+    PrintStr(&s3);
+
+    //compare
+    StrAssign(&s2, "Hello");
+    printf("Compare s1 and s2: %d\n", StrCompare(&s1, &s2));
+
+    // substring
+    SubStr(&s3, &s2, 0, 2);
+    PrintStr(&s3);
+
+    // concat
+    ClearStr(&s3);
+    Concat(&s3, &s1, &s2);
+    PrintStr(&s3);
+
     return 0;
 }
+
+void InitStr(PtrS S){
+    S->Data[0] = '\0';
+    S->Length = 0;
+}
+bool StrEmpty(PtrS S){
+    return S->Length == 0;
+}
+int StrLength(PtrS S){
+    return S->Length;
+}
+void PrintStr(PtrS S){
+    printf("Enter to String: ");
+    char* p = S->Data;
+    while(*p != '\0'){
+        printf("%c", *p);
+        p++;
+    }
+    printf("\n");
+}
+
+void InputStr(PtrS S){
+    char c;
+    int i = 0;
+    while((c = getchar()) != '\n' && i < MAXSIZE - 1){
+        S->Data[i] = c;
+        i++;
+    }
+    S->Data[i] = '\0';
+    S->Length = i;
+}
+void ClearStr(PtrS S){
+    InitStr(S);
+}
+
+
+bool StrAssign(PtrS S, char* str){
+    int i = 0;
+    while(str[i] != '\0' && i < MAXSIZE-1){
+        S->Data[i] = str[i];
+        i++;
+    }
+    S->Data[i] = '\0';
+    S->Length = i;
+    return true;
+}
+bool StrCopy(PtrS S, PtrS T){
+    if(S->Length != T->Length){
+        return false;
+    }
+    int i = 0;
+    while(i < S->Length){
+        T->Data[i] = S->Data[i];
+    }
+    return true; 
+}
+int StrCompare(PtrS S, PtrS T){
+    int i=0;
+    for(i = 0; i < S->Length && i < T->Length; i++){
+        if(S->Data[i] != T->Data[i]) break;
+    }
+    return S->Data[i] - T->Data[i];
+}
+
+bool SubStr(PtrS Sub, PtrS S, int pos, int len){
+    if(pos< 0 || pos >= S->Length || len < 0 || len > S->Length - pos){
+        return false;
+    }
+    for(int i=0; i<len; i++){
+        Sub->Data[i] = S->Data[pos+i];
+    }
+    return true;
+}
+
+bool Concat(PtrS S, PtrS S1, PtrS S2){
+    if(S1->Length + S2->Length > MAXSIZE - 1){
+        return false;
+    }
+    for(int i=0; i<S1->Length; i++){
+        S->Data[i] = S1->Data[i];
+    }
+    for(int i=0; i<S2->Length; i++){
+        S->Data[S1->Length + i] = S2->Data[i];
+    }
+    return true;
+}
+
+
--- DataStructure/1-LinearList/1-6 String/1-6-2 DynamicAlloc-String.c
+++ DataStructure/1-LinearList/1-6 String/1-6-2 DynamicAlloc-String.c
@@ -1,2 +1,150 @@
 // 1-6-2 DynamicAlloc-String.c
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#define MAXSIZE 20
+typedef struct{
+    char* Data;
+    int Length;
+}Str, *PtrS;
+
+//functions:
+PtrS InitStr();
+bool StrEmpty(PtrS S);
+int StrLength(PtrS S);
+void PrintStr(PtrS S);
+void InputStr(PtrS S);
+void ClearStr(PtrS S);
+bool StrAssign(char* str, PtrS S);
+bool StrCopy(PtrS S, PtrS T);
+int StrCompare(PtrS S, PtrS T);
+bool Concat(PtrS S, PtrS S1, PtrS S2);
+// int Index(PtrS S, PtrS T);   // 返回子串T在主串S中的位置，若不存在则返回0
+
+int main(){
+    // Initialize strings
+    PtrS s1 = InitStr();
+    PtrS s2 = InitStr();
+    PtrS s3 = InitStr();
+
+    // Input and print strings
+    InputStr(s1);
+    PrintStr(s1);
+    printf("Length of s1: %d\n", StrLength(s1));
+    InputStr(s2);
+    PrintStr(s2);
+    printf("Length of s2: %d\n", StrLength(s2));
+
+    //copy
+    StrCopy(s1, s3);
+    PrintStr(s3);
+    StrCopy(s2, s3);
+    PrintStr(s3);
+
+    //compare
+    printf("Compare s1 and s2: %d\n", StrCompare(s1, s2));
+
+    // concat
+    Concat(s3, s1, s2);
+    PrintStr(s3);
+
+
+    return 0;
+}
+
+PtrS InitStr(){
+    PtrS S = (PtrS)malloc(sizeof(Str));
+    S->Data = NULL;
+    S->Length = 0;
+    return S;
+}
+bool StrEmpty(PtrS S){
+    return S->Length == 0;
+}
+int StrLength(PtrS S){
+    return S->Length;
+}
+void PrintStr(PtrS S){
+    printf("Enter to String: ");
+    char* p = S->Data;
+    while(*p != '\0'){
+        printf("%c", *p);
+        p++;
+    }
+    printf("\n");
+}
+void InputStr(PtrS S){
+    printf("Enter to String: ");
+    char c;
+    int size = 0;
+    char temp[MAXSIZE];
+    while((c = getchar()) != '\n' && size < MAXSIZE - 1){
+        temp[size] = c;
+        size++;
+    }
+    temp[size] = '\0';
+    StrAssign(S, &temp);
+}
+bool StrAssign(char* str, PtrS S){
+    if(S==NULL) {
+        S= (PtrS)malloc(sizeof(Str));
+    }
+    free(S->Data);
+    S->Data = (char*)malloc(MAXSIZE * sizeof(char));
+    int i = 0;
+    for(i = 0; str[i] != '\0'; i++) {
+        if (i >= MAXSIZE - 1) break; // Exceeds maximum size
+        S->Data[i] = str[i];
+    }
+    S->Length = i;
+    S->Data[i] = '\0';
+    return true;
+}
+bool StrCopy(PtrS S, PtrS T){
+    if(S->Length < T->Length){
+        return false;
+    }
+    int i = 0;
+    while(i < S->Length){
+        T->Data[i] = S->Data[i];
+        i++;
+    }
+    T->Data[i] = '\0';
+    T->Length = S->Length;
+    return true;
+}
+int StrCompare(PtrS S, PtrS T){
+    int i=0;
+    for(i = 0; i < S->Length && i < T->Length; i++)
+    if(S->Data[i] != T->Data[i]) break;
+    return S->Data[i] - T->Data[i];
+}
+bool Concat(PtrS S, PtrS S1, PtrS S2){
+    int sameLen = S1->Length + S2->Length;
+    if(sameLen > MAXSIZE - 1){
+        return false; // Concatenated string exceeds maximum size
+    }
+    S->Data = (char*)malloc((sameLen + 1) * sizeof(char));
+    if(S->Data == NULL){
+        return false; // Memory allocation failed
+    }
+    int i;
+    for(i = 0; i < S1->Length; i++){
+        S->Data[i] = S1->Data[i];
+    }
+    for(int j = 0; j < S2->Length; j++, i++){
+        S->Data[i] = S2->Data[j];
+    }
+    S->Data[sameLen] = '\0';
+    S->Length = sameLen;
+    return true;
+}
+void ClearStr(PtrS S){
+    if(S->Data != NULL){
+        free(S->Data);
+        S->Data = NULL;
+    }
+    S->Data = NULL;
+    S->Length = 0;
+}
