c3b986940f9246e49d234c50f0733747a648e87f
Alioth <alioth21@qq.com>
2026-08-22 02:02:05 +0800

    static link list update

--- DataStructure/1-LinearList/1-2 LinkList/1-2-5 AoS-StaticLinkList.c
+++ DataStructure/1-LinearList/1-2 LinkList/1-2-5 AoS-StaticLinkList.c
@@ -2,6 +2,7 @@
 // Array of Structures, AoS
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #define NODATA -1
 #define MAXSIZE 5
@@ -15,14 +16,14 @@ struct Node {
     int Next;
 };
 
-struct SList{
+struct SList{   // Array of Structures, AoS
     struct Node Data[MAXSIZE];
     int Length;
     int Head, Tail;
 };
 
 //
-void InitList(PtrL L);
+PtrL InitList();
 void PrintList(PtrL L);
 bool InsertNode(PtrL L);
 bool FindNode(PtrL L);
@@ -33,8 +34,8 @@ void ClearList(PtrL L);
 bool IsEmpty(PtrL L);
 bool IsFull(PtrL L);
 int GetLength(PtrL L);
-int MallocNode(PtrL L);
-void FreeNode(PtrL L, int index);
+Ptr GetNode(PtrL L, int index);
+int findFreeIndex(PtrL L);
 bool InserHead(PtrL L, DataType data);
 bool InsertTail(PtrL L, DataType data);
 bool InsertAt(PtrL L, DataType data, int index);
@@ -44,11 +45,9 @@ bool DeleteAt(PtrL L, int index);
 
 int main(){
     int choice = -1;
-    struct SList List;
-    PtrL L = &List;
-    InitList(L);
+    PtrL L = InitList();
 
-    while (choice)  {
+    while (choice!=0)  {
         printf("1. PrintList\n");
         printf("2. Length\n");
         printf("3. Insert\n");
@@ -67,6 +66,7 @@ int main(){
             case 5: UpdateNode(L); break;
             case 6: DeleteNode(L); break;
             case 7: ClearList(L); break;
+            case 0: break;
             default: printf("Invalid input!\n");
         }
     }
@@ -74,36 +74,133 @@ int main(){
     return 0;
 }
 
-void InitList(PtrL L){
+PtrL InitList(){
+    PtrL L = (PtrL)malloc(sizeof(struct SList));
     for(int i=0; i<MAXSIZE; i++){
         L->Data[i].NodeData = NODATA;
+        L->Data[i].Next = -1;
     }
     L->Head = -1;
     L->Tail = -1;
     L->Length = 0;
+    return L;
 }
 void PrintList(PtrL L){
-    int index = L->Head;
-    while(index != -1){
-        printf("%d ", L->Data[index].NodeData);
-        index = L->Data[index].Next;
+    printf("Storage of List:\n");
+    printf("Index, Data, Next\n");
+    for(int i=0; i<MAXSIZE; i++){
+        printf(" %d, Data: %d, Next: %d\n", i, L->Data[i].NodeData, L->Data[i].Next);
+    }
+    printf("List : ");
+    int i = L->Head;
+    while(i != -1){
+        printf("%d ", L->Data[i].NodeData);
+        i = L->Data[i].Next;
+        if(i == -1 || L->Data[i].NodeData == NODATA ){
+            break;
+        }
     }
     printf("\n");
 }
+Ptr GetNode(PtrL L, int index){
+    if(index<0 || index>=L->Length){
+        return NULL;
+    }
+    int pos = L->Head;
+    for(int i=0; i<index; i++){
+        pos = L->Data[pos].Next;
+    }
+    return &L->Data[pos];
+}
+
+bool FindNode(PtrL L){
+    int index=0;
+    printf("Enter the index to find: ");
+    scanf("%d", &index);
+    if(index<0 || index>=L->Length){
+        printf("Invalid index!\n");
+        return false;
+    }
+    Ptr p = GetNode(L, index);
+    if(p == NULL){
+        printf("Node not found!\n");
+        return false;
+    }
+    printf("Element at index %d: %d\n", index, p->NodeData);
+    return true;
+}
+bool UpdateNode(PtrL L){
+    if(IsEmpty(L)){
+        printf("List is empty!\n");
+        return false;
+    }
+    int index=0;
+    DataType data;
+    printf("Enter the index to update: ");
+    scanf("%d", &index);
+    printf("Enter the new element: ");
+    scanf("%d", &data);
+    if(index<0 || index>=L->Length){
+        printf("Invalid index!\n");
+        return false;
+    }
+    Ptr p = GetNode(L, index);
+    if(p == NULL){
+        printf("Node not found!\n");
+        return false;
+    }
+    p->NodeData = data;
+    return true;
+}
 bool InsertNode(PtrL L){
-    int index;
+    if(IsFull(L)) {
+        printf("List is full!\n");
+        return false;
+    }
+    int index=0;
     DataType data;
     printf("Enter the index to insert: ");
     scanf("%d", &index);
     printf("Enter the element to insert: ");
     scanf("%d", &data);
-
+    if(index<0 || index>L->Length){
+        printf("Invalid index!\n");
+        return false;
+    }
+    if(index == 0){
+        return InserHead(L, data);
+    }
+    else if(index == L->Length){
+        return InsertTail(L, data);
+    }
+    else{
+        return InsertAt(L, data, index);
+    }
+    return true;
 }
-bool FindNode(PtrL L){
-    
+bool DeleteNode(PtrL L){
+    if(IsEmpty(L)){
+        printf("List is empty!\n");
+        return false;
+    }
+    int index=0;
+    printf("Enter the index to delete: ");
+    scanf("%d", &index);
+    if(index<0 || index>=L->Length){
+        printf("Invalid index!\n");
+        return false;
+    }
+    if(index == 0){
+        DeleteHead(L);
+    }
+    else if(index == L->Length - 1){
+        DeleteTail(L);
+    }
+    else{
+        DeleteAt(L, index);
+    }
+    return true;
 }
-bool UpdateNode(PtrL L);
-bool DeleteNode(PtrL L);
 void ClearList(PtrL L){
     while (!IsEmpty(L)){
         DeleteHead(L);
@@ -119,12 +216,125 @@ bool IsFull(PtrL L){
 int GetLength(PtrL L){
     return L->Length;
 }
-int MallocNode(PtrL L);
-void FreeNode(PtrL L, int index);
-bool InserHead(PtrL L, DataType data);
-bool InsertTail(PtrL L, DataType data);
-bool InsertAt(PtrL L, DataType data, int index);
-bool DeleteHead(PtrL L);
-bool DeleteTail(PtrL L);
-bool DeleteAt(PtrL L, int index);
+int findFreeIndex(PtrL L){
+    for(int i=0; i<MAXSIZE; i++){
+        if(L->Data[i].NodeData == NODATA){
+            return i;
+        }
+    }
+    return -1; // No free index found
+}
+
+bool InserHead(PtrL L, DataType data){
+    if (IsFull(L)) {
+        return false;
+    }
+    int index = findFreeIndex(L);
+    if(index == -1){
+        return false;
+    }
+    L->Data[index].NodeData = data;
+    L->Data[index].Next = L->Head;
+    L->Head = index;
+    if(L->Length == 0){
+        L->Tail = index;
+    }
+    L->Length++;
+    return true;
+}
+bool InsertTail(PtrL L, DataType data){
+    if (IsFull(L)) {
+        return false;
+    }
+    int index = findFreeIndex(L);
+    if(index == -1){
+        return false;
+    }
+    L->Data[index].NodeData = data;
+    L->Data[index].Next = -1;
+    if(L->Length == 0){
+        L->Head = index;
+        L->Tail = index;
+    }
+    else{
+        L->Data[L->Tail].Next = index;
+        L->Tail = index;
+    }
+    L->Length++;
+    return true;
+}
+bool InsertAt(PtrL L, DataType data, int index){
+    if(IsFull(L)){
+        return false;
+    }
+    int prevIndex = L->Head;
+    for(int i=0; i<index-1; i++){
+        prevIndex = L->Data[prevIndex].Next;
+    }
+    int newIndex = findFreeIndex(L);
+    if(newIndex == -1){
+        return false;
+    }
+    L->Data[newIndex].NodeData = data;
+    L->Data[newIndex].Next = L->Data[prevIndex].Next;
+    L->Data[prevIndex].Next = newIndex;
+    L->Length++;
+    return true;
+}
+bool DeleteHead(PtrL L){
+    if(IsEmpty(L)){
+        return false;
+    }
+    int oldHead = L->Head;
+    L->Head = L->Data[L->Head].Next;
+    L->Data[oldHead].NodeData = NODATA; // Mark as free
+    L->Data[oldHead].Next = -1;         // Reset next pointer
+    L->Length--;
+    if(L->Length == 0){
+        L->Head = -1; // Reset head if list is empty
+        L->Tail = -1; // Reset tail if list is empty
+    }
+    return true;
+}
+bool DeleteTail(PtrL L){
+    if(IsEmpty(L)){
+        return false;
+    }
+    if(L->Head==L->Tail){ // Only one element
+        L->Data[L->Tail].NodeData = NODATA; // Mark as free
+        L->Data[L->Tail].Next = -1;         // Reset next pointer
+        L->Head = -1;
+        L->Tail = -1;
+        L->Length--;
+        return true;
+    }
+    int prev = L->Head;
+    while(prev != -1){
+        if( L->Data[prev].Next == L->Tail){
+            L->Data[prev].Next = -1; // Set the next of the previous node to -1
+            break;
+        }
+        prev = L->Data[prev].Next;
+    }
+    L->Data[L->Tail].NodeData = NODATA; // Mark as free
+    L->Data[L->Tail].Next = -1;         // Reset next pointer
+    L->Tail = prev; // Update tail to the previous node
+    L->Length--;
+    return true;
+}
+bool DeleteAt(PtrL L, int index){
+    if (IsEmpty(L) || index<0 || index>=L->Length){
+        return false;
+    }
+    int prev = L->Head;
+    for(int i=0; i<index-1; i++){
+        prev = L->Data[prev].Next;
+    }
+    int del = L->Data[prev].Next;
+    L->Data[prev].Next = L->Data[del].Next;
+    L->Data[del].NodeData = NODATA; // Mark as free
+    L->Data[del].Next = -1;         // Reset next pointer
+    L->Length--;
+    return true;
+}
 
--- DataStructure/1-LinearList/1-2 LinkList/1-2-6 SoA-StaticLinkList.c
+++ DataStructure/1-LinearList/1-2 LinkList/1-2-6 SoA-StaticLinkList.c
@@ -4,43 +4,45 @@
 #include <stdio.h>
 #include <stdbool.h>
 #define MAXSIZE 5
+#define NO_DATA -1
 
 typedef int DataType;
 
 typedef struct{
     DataType Data[MAXSIZE];
-    int Index[MAXSIZE];
+    int Next[MAXSIZE];
     int Length;
     int Head, Tail;
-}SList;
+}SList, *PtrL;
 
-void InitList(SList* L);
-void PrintList(SList* L);
-bool InsertNode(SList* L);
-bool FindNode(SList* L);
-bool UpdateNode(SList* L);
-bool DeleteNode(SList* L);
-void ClearList(SList* L);
+//
+bool InitList(PtrL L);
+void PrintList(PtrL L);
+bool InsertNode(PtrL L);
+bool FindNode(PtrL L);
+bool UpdateNode(PtrL L);
+bool DeleteNode(PtrL L);
+void ClearList(PtrL L);
 
-bool IsEmpty(SList* L);
-bool IsFull(SList* L);
-int GetLength(SList* L);
-int MallocNode(SList* L);
-void FreeNode(SList* L, int index);
-bool InsertHead(SList* L, DataType data);
-bool InsertTail(SList* L, DataType data);
-bool InsertAt(SList* L, DataType data, int index);
-bool DeleteHead(SList* L);
-bool DeleteTail(SList* L);
-bool DeleteAt(SList* L, int index);
-
+bool IsEmpty(PtrL L);
+bool IsFull(PtrL L);
+int GetLength(PtrL L);
+int GetIndex(PtrL L, int index);
+int findFreeIndex(PtrL L);
+bool InserHead(PtrL L, DataType data);
+bool InsertTail(PtrL L, DataType data);
+bool InsertAt(PtrL L, DataType data, int index);
+bool DeleteHead(PtrL L);
+bool DeleteTail(PtrL L);
+bool DeleteAt(PtrL L, int index);
 
 int main(){
     int choice = -1;
-    SList List;
-    InitList(&List);
+    SList list;
+    PtrL L = &list;
+    InitList(L);
 
-    while (choice)  {
+    while (choice!=0)  {
         printf("1. PrintList\n");
         printf("2. Length\n");
         printf("3. Insert\n");
@@ -50,37 +52,252 @@ int main(){
         printf("7. Clear\n");
         printf("0. Exit\n");
         printf("Enter your choice: ");
-        scanf("%d", &choice);
+        if(scanf("%d", &choice) != 1){
+            while(getchar() != '\n');
+            choice = -1;
+            printf("Invalid input!\n");
+            continue;
+        }
         switch (choice){
-        case 1: PrintList(&List);   break;
-        case 2: printf("Length: %d\n", GetLength(&List)); break;
-        case 3: InsertNode(&List);   break;
-        case 4: FindNode(&List);     break;
-        case 5: UpdateNode(&List);   break;
-        case 6: DeleteNode(&List);   break;
-        case 7: ClearList(&List);    break;
-        default:printf("Invalid input!\n");
+            case 1: PrintList(L); break;
+            case 2: printf("Length: %d\n", GetLength(L)); break;
+            case 3: InsertNode(L); break;
+            case 4: FindNode(L); break;
+            case 5: UpdateNode(L); break;
+            case 6: DeleteNode(L); break;
+            case 7: ClearList(L); break;
+            case 0: break;
+            default: printf("Invalid input!\n");
         }
     }
+
     return 0;
 }
 
-void InitList(SList* L);
-void PrintList(SList* L);
-bool InsertNode(SList* L);
-bool FindNode(SList* L);
-bool UpdateNode(SList* L);
-bool DeleteNode(SList* L);
-void ClearList(SList* L);
+bool InitList(PtrL L){
+    if(L == NULL){
+        return false;
+    }
+    for(int i=0; i<MAXSIZE; i++){
+        L->Data[i] = NO_DATA;
+        L->Next[i] = -1;
+    }
+    L->Head = -1;
+    L->Tail = -1;
+    L->Length = 0;
+    return L;
+}
+void PrintList(PtrL L){
+    printf("Storage of List:\n");
+    printf("Index, Data, Next\n");
+    for(int i=0; i<MAXSIZE; i++){
+        printf(" [%d] Data: %d, Next: %d\n", i, L->Data[i], L->Next[i]);
+    }
+    printf("List : ");
+    for(int i=0; i<L->Length; i++){
+        printf("%d ", L->Data[GetIndex(L, i)]);
+    }
+    printf("\n");
+}
+int GetIndex(PtrL L, int index){
+    if(L==NULL || index<0 || index>=L->Length){
+        return -1;
+    }
+    int pos = L->Head;
+    for(int i=0; i<index; i++){
+        pos = L->Next[pos];
+    }
+    return pos;
+}
 
-bool IsEmpty(SList* L);
-bool IsFull(SList* L);
-int GetLength(SList* L);
-int MallocNode(SList* L);
-void FreeNode(SList* L, int index);
-bool InsertHead(SList* L, DataType data);
-bool InsertTail(SList* L, DataType data);
-bool InsertAt(SList* L, DataType data, int index);
-bool DeleteHead(SList* L);
-bool DeleteTail(SList* L);
-bool DeleteAt(SList* L, int index);
+bool FindNode(PtrL L){
+    int index=0;
+    printf("Enter the index to find: ");
+    scanf("%d", &index);
+    if(index<0 || index>=L->Length){
+        printf("Invalid index!\n");
+        return false;
+    }
+    int pos = GetIndex(L, index);
+    printf("Element at index %d pos[%d] Data=%d Next=%d\n", index, pos, L->Data[pos], L->Next[pos]);
+    return true;
+}
+bool UpdateNode(PtrL L){
+    if(IsEmpty(L)){
+        printf("List is empty!\n");
+        return false;
+    }
+    int index=0;
+    DataType data;
+    printf("Enter the index to update: ");
+    scanf("%d", &index);
+    printf("Enter the new element: ");
+    scanf("%d", &data);
+    if(index<0 || index>=L->Length){
+        printf("Invalid index!\n");
+        return false;
+    }
+    int pos = GetIndex(L, index);
+    if(pos == -1){
+        printf("Node not found!\n");
+        return false;
+    }
+    L->Data[pos] = data;
+    return true;
+}
+bool InsertNode(PtrL L){
+    if(IsFull(L)) {
+        printf("List is full!\n");
+        return false;
+    }
+    int index=0;
+    DataType data;
+    printf("Enter the index to insert: ");
+    scanf("%d", &index);
+    printf("Enter the element to insert: ");
+    scanf("%d", &data);
+    if(index<0 || index>L->Length){
+        printf("Invalid index!\n");
+        return false;
+    }
+    if(index == 0){
+        return InserHead(L, data);
+    }
+    else if(L->Length !=0 && index == L->Length){
+        return InsertTail(L, data);
+    }
+    else{
+        return InsertAt(L, data, index);
+    }
+    return true;
+}
+bool DeleteNode(PtrL L){
+    if(IsEmpty(L)){
+        printf("List is empty!\n");
+        return false;
+    }
+    int index=0;
+    printf("Enter the index to delete: ");
+    scanf("%d", &index);
+    if(index<0 || index>=L->Length){
+        printf("Invalid index!\n");
+        return false;
+    }
+    if(index == 0){
+        DeleteHead(L);
+    }
+    else if(index!=0 && index == L->Length-1 ){
+        DeleteTail(L);
+    }
+    else{
+        DeleteAt(L, index);
+    }
+    return true;
+}
+void ClearList(PtrL L){
+    while (!IsEmpty(L)){
+        DeleteHead(L);
+    }
+}
+
+bool IsEmpty(PtrL L){
+    return (L->Length == 0);
+}
+bool IsFull(PtrL L){
+    return (L->Length == MAXSIZE);
+}
+int GetLength(PtrL L){
+    return L->Length;
+}
+int findFreeIndex(PtrL L){
+    for(int i=0; i<MAXSIZE; i++){
+        if(L->Data[i] == NO_DATA){
+            return i;
+        }
+    }
+    return -1; // No free index found
+}
+
+bool InserHead(PtrL L, DataType data){
+    int pos = findFreeIndex(L);
+    if(pos == -1){
+        return false;
+    }
+    L->Data[pos] = data;
+    L->Next[pos] = L->Head;
+    L->Head = pos;
+    if(L->Length == 0){
+        L->Tail = pos;
+    }
+    L->Length++;
+    return true;
+}
+bool InsertTail(PtrL L, DataType data){
+    int newpos = findFreeIndex(L);
+    if(newpos == -1){
+        return false;
+    }
+    L->Data[newpos] = data;
+    L->Next[newpos] = -1;
+    L->Next[L->Tail] = newpos; // Link the old tail to the new node
+    L->Tail = newpos;
+    L->Length++;
+    return true;
+}
+bool InsertAt(PtrL L, DataType data, int index){
+    int newpos = findFreeIndex(L);
+    if(newpos == -1){
+        return false;
+    }
+    int prev = L->Head;
+    for(int i=0; i<index-1; i++){
+        prev = L->Next[prev];
+    }
+    L->Data[newpos] = data;
+    L->Next[newpos] = L->Next[prev];
+
+    L->Next[prev] = newpos;    //prev index's next is newpos
+    L->Length++;
+    return true;
+}
+bool DeleteHead(PtrL L){
+    int oldHead = L->Head;
+    L->Head = L->Next[L->Head];
+    L->Data[oldHead] = NO_DATA; // Mark as free
+    L->Next[oldHead] = -1;         // Reset next pointer
+    L->Length--;
+    if(L->Length == 0){
+        L->Head = -1; // Reset head if list is empty
+        L->Tail = -1; // Reset tail if list is empty
+    }
+    return true;
+}
+bool DeleteTail(PtrL L){
+    int prev = L->Head;
+    while(prev != -1){
+        if( L->Next[prev] == L->Tail){
+            L->Next[prev] = -1; // Set the next of the previous node to -1
+            break;
+        }
+        prev = L->Next[prev];
+    }
+    L->Data[L->Tail] = NO_DATA; // Mark as free
+    L->Next[L->Tail] = -1;         // Reset next pointer
+    L->Tail = prev; // Update tail to the previous node
+    L->Length--;
+    return true;
+}
+bool DeleteAt(PtrL L, int index){
+    int prev = L->Head;
+    for(int i=0; i<index-1; i++){
+        prev = L->Next[prev];
+    }
+    int delpos = L->Next[prev];
+    L->Next[prev] = L->Next[delpos];
+    L->Data[delpos] = NO_DATA; // Mark as free
+    L->Next[delpos] = -1;         // Reset next pointer
+    L->Length--;
+    return true;
+}
+
+
